86e9e033d8457ff4d4a7cd5af3941bd0f80d5a81
El Juli <mayorga.julian@gmail.com>
2014-03-08 12:40:30 -0300

    fix(dips): poner mejores ejemplos de js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,12 +25,41 @@ angular.module('anotableApp', [
 .run(function (Dip, Dips) {
   var codigoUno = {
     html: '<div>Hola mundo</div>',
-    css: '.mundo {display:block}',
-    js: 'console.log("hola");'
+    css: '',
+    js: 'console.log("Hola mundo");'
   };
   var dipUno = new Dip('Hola Mundo', codigoUno);
-  var dipDos = new Dip('FizzBuzz', {html: '<h1>FizzBuzz</h1>', css: '', js: ''});
-  var dipTres = new Dip('Bubble sort', {html: '<h1>Bubble Sort</h1>', css: '', js: ''});
+  var dipDos = new Dip('FizzBuzz', {html: '', css: '', js: 'for (var i = 1; i <= 100; i++) {\n' +
+                       '  var f = i % 3 == 0, b = i % 5 == 0;\n' +
+                       '  console.log(f ? b ? "FizzBuzz" : "Fizz" : b ? "Buzz" : i);\n' +
+                       '}'});
+  var dipTres = new Dip('Bubble sort', {html: '', css: '', js:
+  '  function comparator(a, b) {\n' +
+    '    return a - b;\n' +
+    '  }\n' +
+    '\n' +
+    '  /**\n' +
+    '   * The bubblesort algorithm. Complexity O(n^2).\n' +
+    '   *\n' +
+    '   * @public\n' +
+    '   * @param {array} array Input array\n' +
+    '   * @returns {array} array Sorted array\n' +
+    '   */\n' +
+    '  function bubbleSort(array, cmp) {\n' +
+    '    cmp = cmp || comparator;\n' +
+    '    var temp;\n' +
+    '    for (var i = 0; i < array.length; i += 1) {\n' +
+    '      for (var j = i; j > 0; j -= 1) {\n' +
+    '        if (cmp(array[j], array[j - 1]) < 0) {\n' +
+    '          temp = array[j];\n' +
+    '          array[j] = array[j - 1];\n' +
+    '          array[j - 1] = temp;\n' +
+    '        }\n' +
+    '      }\n' +
+    '    }\n' +
+    '    return array;\n' +
+    '  }\n'
+});
   Dips.add(dipUno);
   Dips.add(dipDos);
   Dips.add(dipTres);
